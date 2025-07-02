27894da16ce6f92939d62953ce800db7ce7c7084
Dmytro <dmitry.dvernikov@gmail.com>
2025-07-02 17:33:50 +0300

    add Roboto font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,14 @@
-import {Geist, Geist_Mono} from "next/font/google";
 import "./globals.css";
 import Header from "@/components/Header/Header";
 import Footer from "@/components/Footer/Footer";
 import TanStackProvider from "@/components/TanStackProvider/TanStackProvider";
+import {Roboto} from 'next/font/google';
 
-const geistSans = Geist({
-    variable: "--font-geist-sans",
-    subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-    variable: "--font-geist-mono",
-    subsets: ["latin"],
+const roboto = Roboto({
+    weight: ['400', '700'],
+    subsets: ['latin'],
+    display: 'swap',
+    variable: '--font-roboto',
 });
 
 export const metadata = {
@@ -20,7 +17,7 @@ export const metadata = {
     openGraph: {
         title: 'NoteHub – Зручний менеджер нотаток',
         description: 'Створюйте, шукайте та переглядайте нотатки за допомогою зручного інтерфейсу NoteHub.',
-        url: 'https://https://08-zustand-ten.vercel.app/',
+        url: 'https://08-zustand-ten.vercel.app/',
         images: [
             {
                 url: 'https://ac.goit.global/fullstack/react/notehub-og-meta.jpg',
@@ -41,7 +38,7 @@ export default function RootLayout({
 }>) {
     return (
         <html lang="en">
-        <body className={`${geistSans.variable} ${geistMono.variable}`}>
+        <body className={roboto.variable}>
         <TanStackProvider>
             <Header/>
             {children}
